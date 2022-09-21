fd1fa639c51cf8a5dddd60f331fdf4d513847ff1
luk3w00d <110910280+luk3w00d@users.noreply.github.com>
2022-09-21 15:20:14 +1000

    updated menu and added a way to open image

--- photo_file_handling.py
+++ photo_file_handling.py
@@ -1,25 +1,44 @@
 from simple_term_menu import TerminalMenu 
 from exif import Image
 
-folder_path = 'sample_images'
+
+# img.artist = 'bob the builder'
+# with open(f'{folder_path}/{img_filename}', 'wb') as new_image_file:
+    # new_image_file.write(img.get_file())
+
+# print(img.has_exif)
+# print(folder_path)
+# print(img_filename)
+# print(img_path)
+# print(img.list_all())
 
 
 print('Hi, Welcome to the Photo Organising Application !')
 
 def main():
     print('What would you like to do ?')
-    options = ['Edit new image file', 'Continue editing file', 'exit']
+    options = ['View photo metadata', 'Edit photo metadata', 'exit']
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
     while menu_entry_index != 2:
         if menu_entry_index == 0:
-            print('Perfect lets start editing!')
+            print("Perfect lets check the photo's metadata")
+        
+            folder_path = 'Image'
+            img_filename = input('Type the exact name of the image including .jpg ') # input from user
+            img_path = f'{folder_path}/{img_filename}'
+
+            with open(img_path, 'rb') as img_file:
+                img = Image(img_file)
+                print(img.list_all())
+        
         elif menu_entry_index == 1: 
-            print('Perfect lets continue editing!')
+            print("Perfect lets update the photo's metadata")
         
         print('What would you like to do ?')
         options = ['Add Metadata', 'Remove Metadata', 'exit']
         terminal_menu = TerminalMenu(options)
+        
         menu_entry_index = terminal_menu.show()
         while menu_entry_index != 2:
             if menu_entry_index == 0:
@@ -28,6 +47,11 @@ def main():
                 print('What would you like to Remove?')
 
             menu_entry_index = terminal_menu.show()
+        
+        
+
+
+        
     
 
     print('See you next time !')
