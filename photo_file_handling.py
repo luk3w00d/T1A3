be29bbb941ffcb6e346449d45e62d5ce5c213e90
luk3w00d <110910280+luk3w00d@users.noreply.github.com>
2022-09-23 10:17:08 +1000

    fixed some issues in meanu

--- photo_file_handling.py
+++ photo_file_handling.py
@@ -2,9 +2,10 @@ from simple_term_menu import TerminalMenu
 from exif import Image
 
 
+
 # img.artist = 'bob the builder'
 # with open(f'{folder_path}/{img_filename}', 'wb') as new_image_file:
-    # new_image_file.write(img.get_file())
+#     new_image_file.write(img.get_file())
 
 # print(img.has_exif)
 # print(folder_path)
@@ -18,22 +19,22 @@ print('Hi, Welcome to the Photo Organising Application !')
 
 def main():
     print('What would you like to do ?')
-    options = ['View photo metadata', 'Edit photo metadata', 'exit']
-    terminal_menu = TerminalMenu(options)
-    menu_entry_index = terminal_menu.show()
+    menu_entry_index = -1
     while menu_entry_index != 2:
+        options = ['View photo metadata', 'Edit photo metadata', 'exit']
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
         if menu_entry_index == 0:
             print("Perfect lets check the photo's metadata")
-        
             folder_path = 'Image'
-            img_filename = input('Type the exact name of the image including the file type - .jpg, .png... ') # input from user
+            img_filename = "image1.JPG"
+            # img_filename = input('Type the exact name of the image including the file type  .jpg, .png ') # input from user
             img_path = f'{folder_path}/{img_filename}'
             print('What would you like to view ?')
-
             with open(img_path, 'rb') as img_file:
                 img = Image(img_file)
-                view_options = ['Copywrite Info', 'Date and time Info', 'What device used']
-                terminal_menu = TerminalMenu(view_options)
+                view = ['Copywrite Info', 'Date and time Info', 'What device used']
+                terminal_menu = TerminalMenu(view)
                 view_index = terminal_menu.show()
                 if view_index == 0:
                     print(f'Copyright: {img.get("copyright")}')
@@ -43,22 +44,42 @@ def main():
                 
                 else:
                     print(f'Device Used: {img.get("model")}')    
-            
-                
-                   
-                
                 # ask user what they want to view
                 # Get the input
                 # use input to get the key
                 # then print value
         elif menu_entry_index == 1: 
             print("Perfect lets update the photo's metadata")
-            print('What would you like to do ?')
-            options = ['Add Metadata', 'Remove Metadata', 'exit']
-            terminal_menu = TerminalMenu(options)
-            menu_entry_index = terminal_menu.show()
+            edit = ['Add Metadata', 'Remove Metadata', 'exit']
+            terminal_menu = TerminalMenu(edit)
+            edit_index = terminal_menu.show()
+            if edit_index == 0:
+                    print('What would you like to add ?')
+
+                    metadata = ['copyright info','Artist', 'If a flash was used' ]
+                    folder_path = 'Image'
+                    img_filename = "image1.JPG"
+                    # img_filename = input('Type the exact name of the image including the file type  .jpg, .png ') # input from user
+                    img_path = f'{folder_path}/{img_filename}'
+                    with open(img_path, 'rb') as img_file:
+                        img = Image(img_file)
+                        terminal_menu = TerminalMenu(metadata)
+                        metadata_index = terminal_menu.show()
+                        if metadata_index == 0:
+                            img.copyright = input('Enter new copyright info - ')
+                            print(f'copyright - After: {img.get("copyright")}')
+                        
+
            
-            
+                        # ask glen ^
+
+                   
+                        
+                        
+                    
+                            
+                         
+                
            
              
         else: 
